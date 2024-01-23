14c66982b96cda4108f28958dcf2ba1b04edfc62
jovahnsc <jovanshann@gmail.com>
2024-01-24 00:31:58 +0700

    Update

---
+++ 1-grow.js
@@ -0,0 +1,24 @@
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+// function grow(x) {
+//   let result = x[0];
+
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+
+//   return result;
+// }
+
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+// const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
